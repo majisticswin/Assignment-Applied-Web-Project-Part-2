31a671a47dcca97f3dd25fcbdac363f85295e947
103031430 <103031430@student.swin.edu.au>
2025-10-21 01:23:47 +1100

    feat(jobs): add additional meta tags in head section

--- jobs.php
+++ jobs.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Job Description page with PHP and SQL">
+    <meta name="keywords" content="Job, Description, Roles, PHP, Search, SQL">
+    <meta name="author" content="Disha Anchan">
     <title>Document</title>
 </head>
 <body>
