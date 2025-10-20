ff3f3dae1091fae9526297a8a905c8b54c4d97d1
103031430 <103031430@student.swin.edu.au>
2025-10-21 01:28:49 +1100

    chore(jobs): add indentation for head and body element

--- jobs.php
+++ jobs.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Job Description page with PHP and SQL">
-    <meta name="keywords" content="Job, Description, Roles, PHP, Search, SQL">
-    <meta name="author" content="Disha Anchan">
-    
-    <title>Job Description</title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Job Description page with PHP and SQL">
+        <meta name="keywords" content="Job, Description, Roles, PHP, Search, SQL">
+        <meta name="author" content="Disha Anchan">
+        
+        <title>Job Description</title>
 
-    <link rel="stylesheet" href="styles\jobs_styles.css">
+        <link rel="stylesheet" href="styles\jobs_styles.css">
 
-    <style>
-        main hr {
-        width: 100%;
-        border: 0.5px solid #233142;
-        margin: 2em 0em;
-    }
-    </style>
-    
-</head>
-<body>
-    
-</body>
+        <style>
+            main hr {
+            width: 100%;
+            border: 0.5px solid #233142;
+            margin: 2em 0em;
+        }
+        </style>
+
+    </head>
+    <body>
+
+        
+        
+    </body>
 </html>
