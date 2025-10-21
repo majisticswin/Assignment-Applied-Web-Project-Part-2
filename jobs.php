68cf6c3a7389dc3ed44fb65a7eda09f9fb7d7ffc
103031430 <103031430@student.swin.edu.au>
2025-10-21 12:09:15 +1100

    docs(jobs): add comments to jobs.php

--- jobs.php
+++ jobs.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Meta tags for Job Description Page -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Job Description page with PHP and SQL">
         <meta name="keywords" content="Job, Description, Roles, PHP, Search, SQL">
         <meta name="author" content="Disha Anchan">
         
+        <!-- Title of the tab -->
         <title>Job Description</title>
 
+        <!-- Link HTML to the CSS document -->
         <link rel="stylesheet" href="styles\jobs_styles.css">
 
         <style>
@@ -22,8 +25,11 @@
     </head>
     <body>
 
+        <!-- ============================ HEADER SECTION ============================ -->
+
         <?php include 'header.inc'?>        <!-- Attach the Header Section using the header.inc file for layout consistency  -->
 
+        <!-- ============================ MAIN SECTION ============================== -->
         <main>
             
             <h1>Careers</h1>
@@ -41,6 +47,9 @@
                 <!-- </aside> -->
             <!-- </div> -->
 
+            <!-- ============================ SEARCH BAR SECTION ======================== -->
+            <!-- A form is used to create the Search bar of the job description page  -->
+
             <form action="jobs.php" method="GET">  <!-- The action will change the layout of the job.php to only contain matching title name -->
                 <fieldset>
                     <legend>Job Search</legend>
@@ -50,18 +59,29 @@
                 </fieldset>
             </form>
 
-            <form action="jobs.php" method="get">
+            <!-- ============================ FILTER SECTION ============================ -->
+
+            <!-- A form used to create the Filter section of the job description page  -->
+            <form action="jobs.php" method="get">   <!-- The action will change the layout of the job.php to only contain matching filter options -->
                 <fieldset>
                     <legend>Filter</legend>
 
+                    <!-- ============================ SALARY RANGE FILTER SECTION ============================ -->
+
+                    <!-- Implement Salary range using two input elements: Min. Salary and Max. Salary -->
+                    <!-- Minimum Salary Input element -->
                     <label for="min-salary">Min. Salary: </label>
                     <input type="text" id="min-salary" name="min-salary" value="0" pattern="[0-9]*">        <!-- pattern is needed to only accept numerical values -->
                     <br>
 
+                    <!-- Maximum Salary input element -->
                     <label for="max-salary">Max. Salary: </label>
                     <input type="text" id="max-salary" name="max-salary" value="350000" pattern="[0-9]*">   <!-- value attribute is needed as both minimum and maximum salary value is added into the SQL query --> 
                     <br>
 
+                    <!-- ============================ CATEGORY FILTER SECTION ============================ -->
+
+                    <!-- Category Selection using Dropdown Menu -->
                     <label for="category">Category: </label>
                     <select name="category" id="category">                   <!-- Submits value under the id "category" -->
                         <option value=" ">Select option</option>             <!-- Select option value allows for blank values to be submitted -->
@@ -71,6 +91,9 @@
                     </select>
                     <br>
 
+                    <!-- ============================ CATEGORY FILTER SECTION ============================ -->
+
+                    <!-- Work Type Selection using Dropdown Menu -->
                     <label for="job_type">Work Type: </label>
                     <select name="job_type" id="job_type">                  <!-- Submits value under the id "job_type" -->
                         <option value=" ">Select option</option>
@@ -81,6 +104,9 @@
                     </select>
                     <br>
 
+                    <!-- ============================ LOCATION FILTER SECTION ============================ -->
+
+                    <!-- Location Selection using Dropdown menu -->
                     <label for="location">Location</label>
                     <select name="location" id="location">                  <!-- Submits value under the id "location" -->
                         <option value=" ">Select option</option>
@@ -89,12 +115,20 @@
                     </select>
                     <br>
 
+                    <!-- ============================ FILTER SUBMIT SECTION ============================ -->
+
                     <input type="submit" value="Filter">
 
                 </fieldset>
             </form>
 
+            <!-- ============================ JOB DESCRIPTION RESULTS SECTION ============================ -->
+
             <?php
+
+                // ============================ DATABASE CONNECTION ============================ //
+
+                // ============================ RETRIEVE DATA USING THE FILTER SECTION ============================ //
                 require_once "settings.php";
                 $conn = @mysqli_connect($host,$user,$pwd,$sql_db);
                 if ($conn) {
@@ -111,7 +145,10 @@
                         } else {
                         echo "No matching jobs found.";
                         }
-                    } elseif (isset($_GET['title'])) {
+                    } 
+                    
+                // ============================ RETRIEVE DATA USING THE SEARCH BAR SECTION ============================ //
+                    elseif (isset($_GET['title'])) {
                         $title = mysqli_real_escape_string($conn, $_GET['title']);
                         $query = "SELECT * FROM jobs WHERE title LIKE '%$title%'";
                         $result = mysqli_query($conn, $query);
@@ -120,7 +157,9 @@
                         } else {
                             echo "No matching jobs found.";
                         }
-                    } else {
+                    } 
+                // ============================ DISPLAY ALL DATA FROM SQL ============================ //
+                    else {
                         $query = "SELECT * FROM jobs";
                         $result = mysqli_query($conn, $query);
                         if ($result) {
@@ -137,7 +176,8 @@
             ?>
             
         </main>
+        <!-- ============================ FOOTER SECTION ============================ -->
 
-        <?php include 'footer.inc'; ?>
+        <?php include 'footer.inc'; ?>          <!-- Attach the Footer Section using the footer.inc file for layout consistency  -->
     </body>
 </html>
